5de087eb369b6e4aa5b2ea85d9fd5f01b8f3939a
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-27 18:07:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,15 +4,15 @@ class Student
 
   attr_accessor :id, :name, :grade 
   
-  def initialize (id, name, age)
+  def initialize (id, name, grade)
     @id = nil 
     @name = name
-    @age = age
+    @grade = grade
   end 
   
   def self.create_table 
     sql = <<-SQL 
-      CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade INTEGER)
+      CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, name TEXT, grade TEXT)
       SQL 
     DB[conn].execute(sql)
   end 
