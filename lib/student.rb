336fd954433952cc5bacb9408e5e221fab1b7183
Sehrish Balouch <sehrishbalouch4@gmail.com>
2020-01-27 17:55:33 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,5 +3,14 @@ require_relative "../config/environment.rb"
 class Student
 
   attr_accessor :id, :name, :grade 
+  
+  def initialize (id, name, age)
+    @id = nil 
+    @name = name
+    @age = age
+  end 
+  
+  def create_table 
+    
 
 end
